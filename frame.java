c2a2cb6aa9a2410bc34f79681c4ab7dcbf095c04
JEFFIN SAM GEORGE <76589856+jeffinsamgeorge7@users.noreply.github.com>
2023-03-14 06:46:06 +0530

    Create frame.java

---
+++ frame.java
@@ -0,0 +1,84 @@
+import java.awt.Component;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+#Coonection code!!!!
+  
+ public static Connection myConnection()
+    {
+     Connection con = null;
+
+     try{
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      String connectionUrl="jdbc:mysql://localhost:3306/studreg?"+"user=root&password=root";
+       con = DriverManager.getConnection(connectionUrl);
+       }  
+        catch (ClassNotFoundException ex) {
+            Logger.getLogger(logframe.class.getName()).log(Level.SEVERE, null, ex);
+          }  
+     catch (SQLException ex)
+     {
+     Logger.getLogger(logframe.class.getName()).log(Level.SEVERE, null, ex);
+     }
+      return con;
+     }
+
+#Action button!!!!
+  
+ private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                        
+                   Connection con = myConnection();
+       try{
+           String sql="INSERT INTO reg(`id`,`username`,`password`,`email`) VALUES (?,?,?,?)";
+         
+
+           PreparedStatement prest,prest2;
+           prest = con.prepareStatement(sql);
+         
+           prest.setString(1,text1.getText());
+           prest.setString(2,text2.getText());
+           prest.setString(3,text3.getText());
+           prest.setString(4,text4.getText());
+           prest.executeUpdate();
+           con.close();
+         } catch (SQLException ex) {
+            Logger.getLogger(logframe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // TODO add your handling code here:
+    }
+
+#Table view!!!!
+  
+public void setpersontabledate(){
+      Connection con=myConnection();
+      try{
+           PreparedStatement prest;
+           String sql ="select * from reg";
+           prest= con.prepareStatement(sql);
+           ResultSet rs =prest.executeQuery(sql);
+           
+           
+           while(rs.next()){
+           String id =String.valueOf(rs.getInt("id"));
+           String username=rs.getString("username");
+           String password=rs.getString("password");
+           String email=rs.getString("email");
+           String tbData[]={id,username,password,email};
+           DefaultTableModel tblModel=(DefaultTableModel)tabledb.getModel();//table variable name
+            tblModel.addRow(tbData);
+
+           }
+           
+      }
+      catch (SQLException ex) {
+            Logger.getLogger(logframe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+     }
